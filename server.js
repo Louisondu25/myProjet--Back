fd85c91ef0045df6bcb946e131b8e8b46b76bfaa
Louisondu25 <louisondu25@gmail.com>
2024-09-03 16:31:10 +0200

    Modification du server avec Cors

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ const config = require("./config");
 // Crée une instance de l'application Express
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 //Demarrage de la database
 require("./utils/database");
 const passport = require('./utils/passport')
